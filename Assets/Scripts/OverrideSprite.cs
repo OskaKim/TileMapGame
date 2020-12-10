521e02ee5b712ad42cec40aa8d20c106dd5f6bac
Kim Dohyeong <o2kim0225@naver.com>
2020-12-11 02:15:16 +0900

    override sprite for change animation image

---
+++ Assets/Scripts/OverrideSprite.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class OverrideSprite : MonoBehaviour
+{
+    [SerializeField] private Texture overrideTexture = null;
+
+    private SpriteRenderer spriteRenderer;
+    private static int idMainTex = Shader.PropertyToID("_MainTex");
+    private MaterialPropertyBlock materialPropertyBlock;
+
+    void Awake()
+    {
+        materialPropertyBlock = new MaterialPropertyBlock();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.GetPropertyBlock(materialPropertyBlock);
+        ChangeTexture();
+    }
+
+    void LateUpdate()
+    {
+        spriteRenderer.SetPropertyBlock(materialPropertyBlock);
+    }
+
+    void OnValidate()
+    {
+        if (materialPropertyBlock != null)
+        {
+            ChangeTexture();
+        }
+    }
+
+    void ChangeTexture()
+    {
+        materialPropertyBlock.SetTexture(idMainTex, overrideTexture);
+    }
+}
