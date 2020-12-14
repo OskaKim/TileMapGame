6e15bc3d848f24e99c31dec0d6dd14a3630e8b77
Kim Dohyeong <o2kim0225@naver.com>
2020-12-14 22:35:23 +0900

    add temporary simple inventory

---
+++ Assets/Scripts/UI/UIInventory.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIInventory : MonoBehaviour
+{
+    [SerializeField] GameObject InventoryUIObject;
+    [SerializeField] GameObject OpenInventoryUIButtonObject;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            if(InventoryUIObject.activeSelf)
+            {
+                CloseInventory();
+            }
+            else
+            {
+                OpenInventory();
+            }
+        }
+    }
+
+    public void OpenInventory()
+    {
+        InventoryUIObject.SetActive(true);
+        OpenInventoryUIButtonObject.SetActive(false);
+    }
+
+    public void CloseInventory()
+    {
+        InventoryUIObject.SetActive(false);
+        OpenInventoryUIButtonObject.SetActive(true);
+    }
+}
