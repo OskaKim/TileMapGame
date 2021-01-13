5ac7b67f16c87277d6aaf4ec0b9ee2e0fd7b0351
Dohyeong Kim <o2kim0225@naver.com>
2021-01-13 15:54:35 +0900

    json예시용 코드 삭제

--- Assets/Scripts/IO/JsonIOManager.cs
+++ Assets/Scripts/IO/JsonIOManager.cs
@@ -5,56 +5,6 @@ using UnityEngine;
 
 namespace GameData.Json
 {
-    // 예시용 클래스. Json 활용하기 시작하면 지울거임
-    [System.Serializable]
-    public class JTestClass
-    {
-        public int i;
-        public float f;
-        public bool b;
-        public Vector3 v;
-        public string str;
-        public int[] iArray;
-        public List<int> iList = new List<int>();
-
-        public JTestClass() { }
-        public JTestClass(bool isSet)
-        {
-            if (isSet)
-            {
-                i = 10;
-                f = 99.9f;
-                b = true;
-                v = new Vector3(39.56f, 21.2f, 61f);
-                str = "JSON Test String";
-                iArray = new int[] { 1, 1, 3, 56, 3, 5, 78 };
-
-                for (int idx = 0; idx < 5; idx++)
-                {
-                    iList.Add(2 * idx);
-                }
-            }
-        }
-
-        public void Print()
-        {
-            Debug.Log("i=" + i);
-            Debug.Log("f=" + f);
-            Debug.Log("b=" + b);
-            Debug.Log("v=" + v);
-            Debug.Log("str=" + str);
-
-            for (int idx = 0; idx < iArray.Length; idx++)
-            {
-                Debug.Log(string.Format("iArray[{0}]={1}", idx, iArray[idx]));
-            }
-            for (int idx = 0; idx < iList.Count; idx++)
-            {
-                Debug.Log(string.Format("iList[{0}]={1}", idx, iList[idx]));
-            }
-        }
-    }
-
     // Usage
     /*
     //save
